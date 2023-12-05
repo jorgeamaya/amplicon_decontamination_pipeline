b7e506614a6d2cd35ad393e6c5444da4db388415
jorgeamaya <jeamaya1991@gmail.com>
2023-12-05 15:26:40 +0100

    AmpSeq v.1.0.0

---
+++ Code/Amplicon_TerraPipeline.py
@@ -0,0 +1,348 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import argparse
+import json
+import subprocess
+#import multiprocessing
+
+import amplicon_decontamination as ad
+import asv_to_cigar as ac
+
+def main():
+	"""
+	Implementation of the amplicon decontamination pipeline for use and 
+	distribution in TERRA
+
+	Usage: python Code/Amplicon_TerraPipeline.py --config config.json ...
+	
+	Returns:
+	None. However, see the functions documentation.
+	"""
+
+	### LOAD ARGUMENTS
+
+	#Parse command line arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--config', help="Path to config.json file.", required =True)
+	parser.add_argument('--overlap_reads', action="store_true", help="Specify whether the data contains paired reads that overlap on their targets. For example, MiSeq paired reads of 250bp for 500bp or shorter targets. The pipeline will perform the contamination detection steps with bbmerge or dada2, depending on the choice made be the user.")
+	parser.add_argument('--mixed_reads', action="store_true", help="Specify whether the data contains a mix of reads that overlap and do not overlap on their targets. For example, iSeq paired reads of 150bp for targets that are shorter than 300bp long and targets that are longer than 300bp.")
+	parser.add_argument('--meta', action="store_true", help="Specify if metadata file must be created. This flag runs the pipeline from the beginning.")
+	parser.add_argument('--repo', action="store_true", help="Specify if the reports must be created.")
+	parser.add_argument('--bbmerge', action="store_true", help="Specify if preprocess merge with bbmerge is performed.")
+	parser.add_argument('--bbmerge_report', action="store_true", help="Aggregate bbmerge reports and generate a visual summary. This flag will only work if the --merge and --overlap_reads flags are active.")
+	parser.add_argument('--adaptor_removal', action="store_true", help="Specify if adaptor removal needed.")
+	parser.add_argument('--primer_removal', action="store_true", help="Specify if primer removal needed.")
+	parser.add_argument('--dada2', action="store_true", help="Specifiy if standard preprocess merge with DADA2 is performed.")
+	#parser.add_argument('--dada2_contamination', action="store_true", help="Specifiy if contamination detection is performed with DADA2. Unlike in the standard DADA2 preproprocess, dada2_contamination merges the reads after adaptor removal, not after primer removal. Primer removal will cut the barcodes, making it impossible to perform the matching procedure for contamination detection.")
+	parser.add_argument('--postproc_dada2', action="store_true", help="Specifiy if postProcess of DADA2 results is perfomed.")
+	parser.add_argument('--asv_to_cigar', action="store_true", help="Specifiy if the ASV to CIGAR transformation is perfomed.")
+
+	args = parser.parse_args()
+
+	#Check minimum arguments and contracdicting flags
+	if not any([args.mixed_reads, args.overlap_reads]):
+		sys.exit('Pre-process halted: User must declare the mixed_reads or overlap_reads flags.')
+
+	if args.mixed_reads and args.bbmerge and args.bbmerge_report:
+		sys.exit('Merging of non-overlapping reads with bbmerge unsupported. Use the dada2 and dada2_contamination flags instead. Read documentation for limitations of this method.')
+		
+	#Configuration aguments will be parsed from config.json
+	with open(args.config, 'r') as config_file:
+		config_inputs = json.load(config_file)
+		path_to_fq = config_inputs['path_to_fq']
+		path_to_flist = config_inputs['path_to_flist']
+		if 'pattern_fw' in config_inputs.keys(): pattern_fw = config_inputs['pattern_fw']
+		if 'pattern_rv' in config_inputs.keys(): pattern_rv = config_inputs['pattern_rv']
+		if 'read_maxlength' in config_inputs.keys(): read_maxlength = config_inputs['read_maxlength']
+		if 'pairread_minlength' in config_inputs.keys(): pairread_minlength = config_inputs['pairread_minlength']
+		if 'merge_minlength' in config_inputs.keys(): merge_minlength = config_inputs['merge_minlength']
+		if 'pr1' in config_inputs.keys(): pr1 = config_inputs['pr1']
+		if 'pr2' in config_inputs.keys(): pr2 = config_inputs['pr2']
+		if 'Class' in config_inputs.keys(): Class = config_inputs['Class']
+		if 'maxEE' in config_inputs.keys(): maxEE = config_inputs['maxEE']
+		if 'trimRight' in config_inputs.keys(): trimRight = config_inputs['trimRight']
+		if 'minLen' in config_inputs.keys(): minLen = config_inputs['minLen']
+		if 'truncQ' in config_inputs.keys(): truncQ = config_inputs['truncQ']
+		if 'matchIDs' in config_inputs.keys(): matchIDs = config_inputs['matchIDs']
+		if 'max_consist' in config_inputs.keys(): max_consist = config_inputs['max_consist']
+		if 'omegaA' in config_inputs.keys(): omegaA = config_inputs['omegaA']
+		if 'saveRdata' in config_inputs.keys(): saveRdata = config_inputs['saveRdata']
+		if 'justConcatenate' in config_inputs.keys(): justConcatenate = config_inputs['justConcatenate']
+		if 'maxMismatch' in config_inputs.keys(): maxMismatch = config_inputs['maxMismatch']
+		if 'path_to_DADA2' in config_inputs.keys(): path_to_DADA2 = config_inputs['path_to_DADA2']
+		if 'overlap_pr1' in config_inputs.keys(): overlap_pr1 = config_inputs['overlap_pr1']
+		if 'overlap_pr2' in config_inputs.keys(): overlap_pr2 = config_inputs['overlap_pr2']
+		if 'reference' in config_inputs.keys(): reference = config_inputs['reference']
+		if 'adjust_mode' in config_inputs.keys(): adjust_mode = config_inputs['adjust_mode']
+		if 'path_to_snv' in config_inputs.keys(): path_to_snv = config_inputs['path_to_snv']
+		if 'no_ref' in config_inputs.keys(): no_ref = config_inputs['no_ref']
+		if 'reference2' in config_inputs.keys(): reference2 = config_inputs['reference2']
+		if 'strain' in config_inputs.keys(): strain = config_inputs['strain']
+		if 'strain2' in config_inputs.keys(): strain2 = config_inputs['strain2']
+		if 'polyN' in config_inputs.keys(): polyN = int(config_inputs['polyN'])
+		if 'min_reads' in config_inputs.keys(): min_reads = int(config_inputs['min_reads'])
+		if 'min_samples' in config_inputs.keys(): min_samples = int(config_inputs['min_samples'])
+		if 'max_snv_dist' in config_inputs.keys(): max_snv_dist = int(config_inputs['max_snv_dist'])
+		if 'max_indel_dist' in config_inputs.keys(): max_indel_dist = int(config_inputs['max_indel_dist'])
+		if 'include_failed' in config_inputs.keys(): include_failed = eval(config_inputs['include_failed'])
+		if 'exclude_bimeras' in config_inputs.keys(): exclude_bimeras = eval(config_inputs['exclude_bimeras'])
+		if 'amp_mask' in config_inputs.keys(): amp_mask = config_inputs['amp_mask']
+		#barcodes_file = config_inputs['barcodes_file']
+		if 'verbose' in config_inputs.keys(): verbose = eval(config_inputs['verbose'])
+
+	### PREPARE OUTPUT DIRECTORIES
+
+	#Generate the path to the Results directory.
+	global res_dir
+	global rep_dir
+	res_dir = os.path.abspath(os.path.join("Results"))
+	rep_dir = os.path.abspath(os.path.join("Report"))
+
+	#Restart Results and Report directory if the workflow runs form the start
+	if args.meta:
+		os.system("rm -rf " + res_dir)
+		os.mkdir(res_dir)
+
+	if args.repo:
+		os.system("rm -rf " + rep_dir)
+		os.mkdir(rep_dir)
+	
+	#Create metadata files
+	if args.meta:
+		ad.flush_dir(res_dir, "Fq_metadata")
+		ad.create_meta(path_to_fq, res_dir, "Fq_metadata", "rawfilelist.tsv", pattern_fw, pattern_rv)
+
+	### EXECUTE
+
+	#Remove adaptors
+	#Most sequences must be adaptor free; just in case, run this step to eliminate any lingering adaptors.
+	if args.adaptor_removal:
+		ad.flush_dir(res_dir, "AdaptorRem")
+		meta = open(os.path.join(res_dir, "Fq_metadata", "rawfilelist.tsv"), 'r')
+		samples = meta.readlines()
+		
+		for sample in samples:
+			slist = sample.split()
+			ad.adaptor_rem(slist[0], slist[1], slist[2], res_dir, "AdaptorRem")
+	
+		ad.create_meta(os.path.join(res_dir, "AdaptorRem"), res_dir, "AdaptorRem", "adaptorrem_meta.tsv",
+			pattern_fw="*_val_1.fq.gz", pattern_rv="*_val_2.fq.gz")
+
+	#Merge forward and reverse reads with bbmerge. Only for reads that overlap.
+	if args.bbmerge and args.overlap_reads:
+		ad.flush_dir(res_dir, "Merge")
+		meta = open(os.path.join(res_dir, "AdaptorRem", "adaptorrem_meta.tsv") , 'r')
+		samples = meta.readlines()
+		for sample in samples:
+			slist = sample.split()
+			ad.mergereads(slist[0], slist[1], slist[2], res_dir, "Merge", read_maxlength, pairread_minlength, merge_minlength)
+
+	#Process merge report and generate RStudio plots
+	if args.bbmerge_report and args.overlap_reads:
+		ad.flush_dir(rep_dir, "Merge")
+		with open(os.path.join(rep_dir, "Merge", "bbmergefields.tsv"), 'a') as f:
+			header = ['SampleID', 'Pairs', 'Joined', 'JoinedP', 'Ambiguous', 'AmbiguousP', 'No_Solution', 'No_SolutionP', 
+				'Too_Short', 'Too_ShortP', 'Avg_Insert', 'Standard_Deviation', 'Mode', 'Insert_range_low', 
+				'Insert_range_up', '90th_pc', '75th', '50th_pc', '25th_pc', '10th_pc']	
+			f.write('\t'.join(header) + '\n')
+		meta = open(os.path.join(res_dir, "Merge", "merge_meta.tsv"), 'r')
+		samples = meta.readlines()
+		
+		for sample in samples:
+			slist = sample.split()
+			ad.extract_bbmergefields(slist[0], slist[1], slist[3], res_dir, rep_dir, "Merge")
+
+	#Remove primers
+	#For a set where all reads have overlap
+	if args.overlap_reads and args.primer_removal:
+		ad.flush_dir(res_dir, "PrimerRem")
+		meta = open(os.path.join(res_dir, "AdaptorRem", "adaptorrem_meta.tsv"), 'r')
+		samples = meta.readlines()
+		
+		for sample in samples:
+			slist = sample.split()
+			ad.trim_primer(slist[0], slist[1], slist[2], res_dir, "PrimerRem", pr1, pr2, "prim")
+
+		ad.create_meta(os.path.join(res_dir,"PrimerRem"), res_dir, "PrimerRem", "primrem_meta.tsv",
+			pattern_fw="*_prim_1.fq.gz", pattern_rv="*_prim_2.fq.gz")
+
+	#For a set that mixes reads with and without overlap
+	if args.mixed_reads and args.primer_removal:
+		ad.flush_dir(res_dir, "PrimerRem")
+		meta = open(os.path.join(res_dir, "AdaptorRem", "adaptorrem_meta.tsv"), 'r')
+
+		#Trim primers off Overlapping short targets and demux them to a different file
+		samples = meta.readlines()
+		for sample in samples:
+			slist = sample.split()
+			ad.trim_primer(slist[0], slist[1], slist[2], res_dir, "PrimerRem", overlap_pr1, overlap_pr2, "mixed_op", True)
+
+		#Metafile for trimmed overlapping target reads
+		ad.create_meta(os.path.join(res_dir, "PrimerRem"), res_dir, "PrimerRem", "mixed_op_prim_meta.tsv",
+			pattern_fw="*_mixed_op_1.fq.gz", pattern_rv="*_mixed_op_2.fq.gz")
+
+		#Metafile for un-trimmed non-op target reads
+		ad.create_meta(os.path.join(res_dir, "PrimerRem"), res_dir, "PrimerRem", "mixed_temp_meta.tsv",
+			pattern_fw="*_temp_1.fq.gz", pattern_rv="*_temp_2.fq.gz")
+		temp_meta = open(os.path.join(res_dir, "PrimerRem", "mixed_temp_meta.tsv"), 'r')
+
+		#Trim primers off second subset of non-op long targets 
+		samples = temp_meta.readlines()	
+		for sample in samples:
+			slist = sample.split()
+			ad.trim_primer(slist[0], slist[1], slist[2], res_dir, "PrimerRem", pr1, pr2, "mixed_nop")
+
+		#Metafile for trimmed non-op target reads
+		ad.create_meta(os.path.join(res_dir, "PrimerRem"), res_dir, "PrimerRem", "mixed_nop_prim_meta.tsv", 
+			pattern_fw="*_mixed_nop_1.fq.gz", pattern_rv="*_mixed_nop_2.fq.gz")
+		temp_meta.close()
+
+	#Perform denoising with DADA2
+	#For a set where all reads have overlap
+	if args.overlap_reads and args.dada2:
+		ad.flush_dir(res_dir, "DADA2", "QProfile")
+		path_to_meta = os.path.join(res_dir, "PrimerRem", "primrem_meta.tsv")
+		ad.run_dada2(path_to_DADA2, path_to_meta, path_to_fq, Class, maxEE, trimRight, minLen, truncQ, matchIDs, max_consist, omegaA, justConcatenate, maxMismatch, saveRdata, res_dir, "DADA2")
+		cmd = ['cp', os.path.join(res_dir, 'DADA2', 'seqtab.tsv'), 
+			os.path.join(res_dir, '.'), 
+			'\\', 
+			'cp', os.path.join(res_dir, 'DADA2', 'ASVBimeras.txt'),
+                        os.path.join(res_dir, '.')
+			]
+
+		proccp = subprocess.Popen(cmd)
+		proccp.wait()
+
+	#For a set that mixes reads with and without overlap
+	if args.mixed_reads and args.dada2:
+		#Run DADA2 on op targets
+		ad.flush_dir(res_dir, "DADA2_OP", "QProfile")
+		path_to_meta = os.path.join(res_dir, "PrimerRem", "mixed_op_prim_meta.tsv")
+		justConcatenate=0	
+		ad.run_dada2(path_to_DADA2, path_to_meta, path_to_fq, path_to_flist, Class, maxEE, trimRight, minLen, truncQ, matchIDs, max_consist, omegaA, justConcatenate, maxMismatch,saveRdata, res_dir, "DADA2_OP")
+		seqtab_op = os.path.join(res_dir, 'DADA2_OP', 'seqtab.tsv')
+		bimera_op = os.path.join(res_dir, 'DADA2_OP', 'ASVBimeras.txt')
+
+		#Run DADA2 on non-op targets
+		ad.flush_dir(res_dir, "DADA2_NOP", "QProfile")
+		path_to_meta = os.path.join(res_dir, "PrimerRem", "mixed_nop_prim_meta.tsv")
+		justConcatenate=1	
+		ad.run_dada2(path_to_DADA2, path_to_meta, path_to_fq, path_to_flist, Class, maxEE, trimRight, minLen, truncQ, matchIDs, max_consist, omegaA, justConcatenate, maxMismatch,saveRdata, res_dir, "DADA2_NOP")
+		seqtab_nop = os.path.join(res_dir, 'DADA2_NOP', 'seqtab.tsv')
+		bimera_nop = os.path.join(res_dir, 'DADA2_NOP', 'ASVBimeras.txt')
+
+		#ASV modification block for non-op targets and merge two ASV tables
+		if reference is not None:
+			adjASV = ['Rscript', os.path.join(path_to_DADA2, 'adjustASV.R'), '-s', seqtab_nop, '-ref', str(reference),
+			'-dist', adjust_mode,
+			'-o', os.path.join(res_dir, 'DADA2_NOP', 'correctedASV.txt')]
+			print(adjASV)
+			procASV = subprocess.Popen(adjASV)
+			procASV.wait()
+			seqtab_corrected = os.path.join(res_dir, 'DADA2_NOP', 'seqtab_corrected.tsv')
+			seqtab = ad.merge_seqtab(seqtab_op, seqtab_corrected)
+			bimera = ad.merge_bimeras(bimera_op, bimera_nop)
+		else:
+			print('--reference file not found. skipping ASV correction..')
+			seqtab = ad.merge_seqtab(seqtab_op, seqtab_nop)
+			bimera = ad.merge_bimeras(bimera_op, bimera_nop)
+
+		seqtab.to_csv(os.path.join(res_dir, 'seqtab.tsv'), sep = "\t", index=False)
+		bimera.to_csv(os.path.join(res_dir, 'ASVBimeras.txt'), sep = "\t", index=False)
+
+	if args.postproc_dada2:		
+		ad.flush_dir(res_dir, "PostProc_DADA2")
+		
+		path_to_seqtab = os.path.join(res_dir, 'seqtab.tsv')
+
+		postProc = ['Rscript', os.path.join(path_to_DADA2, 'postProc_dada2.R'), 
+				'-s', path_to_seqtab, 
+				'-b', os.path.join(res_dir, 'ASVBimeras.txt'),
+				'-snv', os.path.join(path_to_snv),
+				'--indel_filter', '0.895',
+				'-o', os.path.join(res_dir, 'PostProc_DADA2', 'ASVTable.txt'),
+				'--fasta']
+
+		if no_ref == 'True':
+			postProc.extend(['-no_ref'])
+		else:
+			postProc.extend(['--reference', str(reference), '--strain', strain])
+			if reference2 != "":
+				postProc.extend(['--reference2', str(reference2), '--strain2', strain2])
+
+		print(postProc)
+		procASV = subprocess.Popen(postProc)
+		procASV.wait()
+
+	#ASV to CIGAR
+	#Convert ASVs from DADA2 pipeline to pseudo-CIGAR strings.
+	if args.asv_to_cigar:		
+		ad.flush_dir(res_dir, "ASV_to_CIGAR", "alingments")
+
+		path_to_seqtab = os.path.join(res_dir, 'seqtab.tsv')
+		path_to_fasta = os.path.join(res_dir, "PostProc_DADA2", "ASVSeqs.fasta") #Fasta file of ASV sequences from DADA2 pipeline"
+		path_to_table = os.path.join(res_dir, "PostProc_DADA2", "ASVTable.txt") #ASV table from DADA2 pipeline
+		path_to_out = os.path.join(res_dir, "CIGARVariants_Bfilter.out.tsv") #Output seqtab tsv file with amplicon/variant counts
+		path_asv_to_cigar = os.path.join(res_dir, "ASV_to_CIGAR", "ASV_to_CIGAR.out.txt") #Output file for ASV -> CIGAR string table 
+		path_to_amp_db = reference #Amplicon sequence fasta file
+		path_to_alignments = os.path.join(res_dir, "ASV_to_CIGAR", "alingments") #Directory to store ASV alignment files
+
+		print(f"INFO: Loading {path_to_amp_db}")
+		amplicons = ac.parse_amp_db(path_to_amp_db)
+		if not amplicons:
+			print(f"ERROR: No amplicons in {path_to_amp_db}")
+			sys.exit(1)
+
+		#Disabled. Possibly deprecated
+		if os.path.exists(amp_mask):
+			print(f"INFO: Loading {amp_mask}")
+			mask = ac.parse_dustmasker(amp_mask)
+		else:
+			print(f"INFO: No mask data specified.")
+			mask = {}
+
+		print(f"INFO: Loading {path_to_fasta}")
+		asvs = ac.get_asv_seqs(path_to_fasta)
+		if not asvs:
+			print(f"ERROR: No ASV sequences in {path_to_fasta}")
+			sys.exit(1)
+
+		print(f"INFO: Parsing {path_to_table} with total reads >= {min_reads}, samples >= {min_samples}, snv_dist <= {max_snv_dist}, indel_dist <= {max_indel_dist}")
+
+		if include_failed:
+			print("WARNING: Including ASVs that failed post-DADA2 filters! This is not recommended.")
+		else:
+			print("INFO: Excluding ASVs that failed post-DADA2 filters.")
+
+		if exclude_bimeras:
+			print("INFO: Excluding ASVs that DADA2 marked as bimeras.")
+
+		bins = ac.parse_asv_table(path_to_table, min_reads=min_reads, min_samples=min_samples, max_snv_dist=max_snv_dist, max_indel_dist=max_indel_dist, include_failed=include_failed, exclude_bimeras=exclude_bimeras) #This function only matches to the first strain.
+		if not bins:
+			print(f"ERROR: No useable data in {path_to_table}")
+			sys.exit(1)
+
+		print(f"INFO: Writing amplicon fasta files to {path_to_alignments}")
+		ac.write_amplicon_fastas(asvs, bins, amplicons, outdir=path_to_alignments)
+
+		print("INFO: Running MUSCLE aligner on amplicon fasta files. Please wait...")
+		ac.run_muscle(bins, outdir=path_to_alignments)
+
+		print("INFO: Parsing alignments to CIGAR strings")
+		cigars = ac.parse_alignments(bins, mask=mask, min_homopolymer_length=polyN, outdir=path_to_alignments, verbose=False)
+		if not cigars:
+			print("ERROR: could not determine CIGAR strings")
+			sys.exit(1)
+
+		if path_asv_to_cigar:
+			ac.write_cigar_strings(cigars, path_asv_to_cigar)
+			print(f"INFO: Wrote ASV->CIGAR table to {path_asv_to_cigar}")
+
+		print(f"INFO: Converting DADA2 seqtab file {path_to_seqtab} to {path_to_out}")
+		if ac.convert_seqtab(path_to_seqtab, cigars, path_to_out):
+			print("INFO: Completed successfully!")
+
+if __name__ == "__main__":
+	main()
---
+++ Code/amplicon_decontamination.py
@@ -0,0 +1,253 @@
+"""
+Amplicon decontamination scripts
+"""
+
+import pandas as pd
+import os, fnmatch
+import subprocess
+import sys
+import shutil
+import glob
+import gzip
+
+#from Bio import SeqIO
+#from Bio.Seq import reverse_complement
+#from Bio.Seq import Seq
+
+#GENERAL USE SCRIPTS SECTION
+def flush_dir(parent_dir, *dirnames):
+	"""
+	Remove all files and subdirectories from a directory, and create a new empty
+	directory with the same name. Multiple subdirectiories may be provided.
+
+	Args:
+	parent_dir (str): The path of the parent directory.
+	dirname (str): The name of the directory to be flushed.
+
+	Returns:
+	None
+	"""
+
+	dirpath = os.path.join(parent_dir, *dirnames)
+	shutil.rmtree(dirpath, ignore_errors=True)
+	os.makedirs(dirpath)
+	return ()
+
+def create_meta(path_to_fq, parent_dir, dirname, filename, pattern_fw, pattern_rv):
+	"""
+	Creates a metadata file with the list of files to process and their paths.
+
+	Args:
+	path_to_fq: string, path to the directory containing the input fastq files
+	parent_dir: string, path to the parent directory where the output files will be stored
+	dirname: string, name of the subdirectory where the output files will be stored
+	filename: string, name of the output metadata file
+	pattern_fw: string, pattern to match forward reads
+	pattern_rv: string, pattern to match reverse reads
+
+	Returns:
+	None
+
+	Example usage:
+
+	create_meta('/path/to/input/fastq/', '/path/to/output/', 'subdir', 'metadata.tsv', '*_R1.fastq', '*_R2.fastq')
+
+	This will search for all files in /path/to/input/fastq/ that end with '_R1.fastq' and '_R2.fastq',
+	create a metadata file named 'metadata.tsv', and store it in a subdirectory named 'subdir' within 
+	/path/to/output/.
+	"""
+
+	filelist = os.listdir(path_to_fq)
+
+	dirpath = os.path.join(parent_dir, dirname) 
+	outfile = os.path.join(dirpath, filename)
+
+	meta_df = []
+	for file_fw in glob.glob(os.path.join(path_to_fq, pattern_fw)):
+		sampleid = os.path.basename(file_fw).split(pattern_fw[1:])[0]
+		file_rv = os.path.join(path_to_fq, sampleid + pattern_rv[1:])
+		if os.path.isfile(file_rv):
+			meta_df.append((sampleid, file_fw, file_rv))
+
+	with open(outfile, 'w') as f:
+		for row in meta_df:
+			f.write('\t'.join(row) + '\n')
+
+	print(f"Meta file generated at location {outfile}")
+	return()
+
+#ADAPTOR AND PRIMER REMOVAL SECTION						
+def adaptor_rem(sampleid, fileF, fileR, res_dir, subdir, qvalue = 5, length = 20): 
+	"""
+	Runs Trim Galore to remove adaptors and trim low-quality reads from paired-end fastq files.
+
+	Args:
+	sampleid (str): The base name for the output files.
+	fileF (str): The path to the forward-read fastq file.
+	fileR (str): The path to the reverse-read fastq file.
+	res_dir (str): The path to the directory where the output files will be saved.
+	subdir (str): The name of the subdirectory within the results directory where output files should be written
+	qvalue (int, optional): The minimum quality score for trimming. Defaults to 5.
+	length (int, optional): The minimum length of the reads to keep after trimming. Defaults to 20.
+
+	Returns:
+	None
+	"""
+
+	if os.path.isfile(fileF) and os.path.isfile(fileR):
+
+		output_dir = os.path.join(res_dir, subdir)
+		cmd = ['trim_galore', '--paired', '--gzip', '--quality', f'{qvalue}', '--length', 
+		f'{length}', '--output_dir', f'{output_dir}', '--basename', f'{sampleid}', f'{fileF}', 
+		f'{fileR}']
+		proc = subprocess.Popen(cmd)
+		proc.wait()
+
+	else:
+		sys.exit('Adaptor Removal halted : one or both of the fastq files not found! Exiting...')
+	return()
+
+def trim_primer(sampleid, fileF, fileR, res_dir, subdir, pr1, pr2, prefix, keep_untrimmed=False):
+	"""
+	Trim primers from paired-end fastq files using cutadapt.
+
+	Args:
+	sampleid (str): Sample identifier.
+	fileF (str): Path to input forward fastq file.
+	fileR (str): Path to input reverse fastq file.
+	res_dir (str): Path to output directory.
+	subdir (str): The name of the subdirectory within the results directory where output files should be written
+	pr1 (str): Path to primer sequence file for forward read.
+	pr2 (str): Path to primer sequence file for reverse read.
+	prefix (str): Prefix to use for output filenames.
+	keep_untrimmed (bool, optional): If True, keep untrimmed reads in separate files. Default is False.
+
+	Returns:
+	None
+	"""
+
+	if os.path.isfile(fileF) and os.path.isfile(fileR):
+
+		cmd = ['cutadapt', '-g', f'file:{pr1}', '-G', f'file:{pr2}',
+			'-o', os.path.join(res_dir, "PrimerRem", f'{sampleid}_{prefix}_1.fq.gz'),
+			'-p', os.path.join(res_dir, "PrimerRem", f'{sampleid}_{prefix}_2.fq.gz'),
+			'--pair-adapters', '--action=trim']
+
+		if keep_untrimmed:
+			cmd.extend(['--untrimmed-output', os.path.join(res_dir, subdir, f'{sampleid}_temp_1.fq.gz'),
+	    			'--untrimmed-paired-output', os.path.join(res_dir, subdir, f'{sampleid}_temp_2.fq.gz')])
+		else:
+			cmd.append('--discard-untrimmed')
+
+		cmd.extend([fileF, fileR])
+
+		proc = subprocess.Popen(cmd)
+		proc.wait()
+
+	else:
+		sys.exit('Pre-process halted : one or both of the fastq files not found! Exiting...')
+	return()
+
+#RUN DADA2 SECTION
+def run_dada2(path_to_DADA2, path_to_meta, path_to_fq, path_to_flist, Class, maxEE, trimRight, minLen, truncQ, matchIDs, max_consist, omegaA, justConcatenate, maxMismatch, saveRdata, res_dir, subdir):
+	"""
+	Runs the DADA2 pipeline on the input files using the specified parameters.
+
+	Args:
+	path_to_DADA2 (str): the path to the DADA2 installation directory.
+	path_to_meta (str): the path to the metadata file containing sample information.
+	path_to_fq (str): the path to the raw fastq.gz files.
+	path_to_flist (str): the path to a csv file with the sample_id,Forward,Reverse, where Forward and Reverse are columns with the barcodes for the sample
+	res_dir (str): the path to the directory where results will be saved.
+	subdir (str): the name of the subdirectory where the output files will be saved.
+	Class (str): the name of the column in the metadata file that contains the sample class information.
+	maxEE (float): the maximum expected error rate.
+	trimRight (int): the number of bases to trim from the 3' end of the reads.
+	minLen (int): the minimum length of reads to retain after trimming.
+	truncQ (int): the quality threshold for truncating reads.
+	matchIDs (bool): boolean to request DADA2 to match ids on fastqs to make sure reads on forward and reverse end are in same order.
+	max_consist (int): the maximum number of mismatches allowed in the overlap region for merging paired-end reads.
+	omegaA (float): the alpha parameter for the consensus quality score.
+	justConcatenate (int): whether to just concatenate the forward and reverse reads without merging them.
+	maxMismatch (int): the maximum number of mismatches allowed during merging.
+	saveRdata (str): whether to save the intermediate R data files.
+
+	Returns:
+	None
+	"""
+
+	if os.path.isfile(path_to_meta):
+
+		if subdir in ['DADA2', 'DADA2_OP', 'DADA2_NOP']:
+			program = 'runDADA2.R'
+		else:
+			program = 'runDADA2contamination.R' 
+
+		bimera = '--bimera'
+		cmd = ['Rscript', os.path.join(path_to_DADA2, program),
+		'-p', f'{path_to_meta}',
+		'-r', f'{path_to_fq}',
+		'-l', f'{path_to_flist}',
+		'-d', os.path.join(res_dir, subdir),
+		'-o', os.path.join(res_dir, subdir, 'seqtab.tsv'),
+		'-c', f'{Class}',
+		'-ee', f'{maxEE}',
+		'-tR', f'{trimRight}',
+		'-mL', f'{minLen}',
+		'-tQ', f'{truncQ}',
+		'-id', f'{matchIDs}',
+		'-mC', f'{max_consist}',
+		'-wA', f'{omegaA}',
+		'-jC', f'{justConcatenate}',
+		'-mM', f'{maxMismatch}',
+		'-s', f'{saveRdata}',
+		f'{bimera}']
+		print(cmd)
+		proc = subprocess.Popen(cmd)
+		proc.wait()
+	else:
+		sys.exit('DADA2 halted : No path to meta file provided! Exiting...')
+	return()
+
+def merge_seqtab(path_op, path_nop):
+	"""
+	Merges overlapping and non-overlapping dada2 tables into a single table.
+
+	Parameters:
+	path_op (str): File path to the overlapping dada2 table in CSV format.
+	path_nop (str): File path to the non-overlapping dada2 table in CSV format.
+
+	Returns:
+	seqtab (pd.DataFrame): Merged dada2 table containing both overlapping and non-overlapping data.
+	"""
+
+	if os.path.isfile(path_op) and os.path.isfile(path_nop):
+		seqtab_op = pd.read_csv(path_op, sep = "\t", index_col=0)
+		seqtab_nop = pd.read_csv(path_nop, sep = "\t", index_col=0)
+		seqtab = seqtab_op.merge(seqtab_nop, left_index=True, right_index=True, how='outer')
+		seqtab.reset_index(inplace=True)
+	else:
+		sys.exit('Overlapping and/or non-overlapping dada2 tables not found! Exiting...')
+
+	return(seqtab)
+
+def merge_bimeras(path_op, path_nop):
+	"""
+	Merges overlapping and non-overlapping bimera tables from dada2 tables.
+
+	Parameters:
+	path_op (str): File path to the overlapping dada2 bimera table in CSV format.
+	path_nop (str): File path to the non-overlapping dada2 bimera table in CSV format.
+
+	Returns:
+	bimeratab (pd.DataFrame): Merged dada2 bimera table containing both overlapping and non-overlapping bimera data.
+	"""
+
+	if os.path.isfile(path_op) and os.path.isfile(path_nop):
+		bimera_op = pd.read_csv(path_op, sep='\t')
+		bimera_nop = pd.read_csv(path_nop, sep='\t')
+		bimeras = pd.concat([bimera_op, bimera_nop], ignore_index=True)
+	else:
+		sys.exit('Overlapping and/or non-overlapping dada2 bimera tables not found! Exiting...')
+
+	return(bimeras)
